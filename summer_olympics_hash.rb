bebf5334e13d2347b4b92e3dc57ef6a4df3a64de
Priya Kumar <priya.kumar08@yahoo.com>
2018-07-25 19:02:27 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -13,6 +13,17 @@ def add_a_key_value_pair
   new_hash
 end
 
+def iterate_through_hash
+  add_a_key_value_pair.each {|key,value|
+    puts "The #{key} summer olympics took place in #{value}."
+  }
+end
+
+def iterate_through_keys
+  upcased_cities = add_a_key_value_pair.map {|key, value| key.upcase  }
+  upcased_cities.each {|city| puts city}
+end
+
 # def iterate_through_hash
 #   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
 #   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
